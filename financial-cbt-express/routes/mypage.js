c121d645b4bc97ece2d066ad2c97725ed754189d
sjun <sammanone@naver.com>
2024-02-26 10:26:35 +0900

    update mypage

--- financial-cbt-express/routes/mypage.js
+++ financial-cbt-express/routes/mypage.js
@@ -25,50 +25,66 @@ router.get('/:userId', async (req, res, next) => {
         
         console.log(newArray)
 
-        let array = [];
-        for (let i = 0; i < newArray.length; i++) {
-            let isDuplicate = false;
-            for (let j = 0; j < array.length; j++) {
-                if (array[j][0] === newArray[i][0]) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (!isDuplicate) {
-                if (newArray[i][1] === true) {
-                    array.push(newArray[i]);
-                }
+        // let array = [];
+        // for (let i = 0; i < newArray.length; i++) {
+        //     let isDuplicate = false;
+        //     for (let j = 0; j < array.length; j++) {
+        //         if (array[j][0] === newArray[i][0]) {
+        //             isDuplicate = true;
+        //             break;
+        //         }
+        //     }
+        //     if (!isDuplicate) {
+        //         if (newArray[i][1] === true) {
+        //             array.push(newArray[i]);
+        //         }
 
-                else {
-                    let check = 0;
-                    for (let k = i+1; k < newArray.length; k++) {
-                        if (newArray[i][0] === newArray[k][0]) {
-                            check = 1;
-                            if(newArray[k][1] === true) {
-                                array.push(newArray[k]);
-                                break
-                            }
-                        }
-                    }
-                    if(check === 0) {
-                        array.push(newArray[i]);
-                    }
-                }
-            }
-        }
+        //         else {
+        //             let check = 0;
+        //             for (let k = i+1; k < newArray.length; k++) {
+        //                 if (newArray[i][0] === newArray[k][0]) {
+        //                     check = 1;
+        //                     if(newArray[k][1] === true) {
+        //                         array.push(newArray[k]);
+        //                         break
+        //                     }
+        //                 }
+        //             }
+        //             if(check === 0) {
+        //                 array.push(newArray[i]);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // let questionArray = []
+        // let accuracyArray = []
+        // let correctNum = 0;
+        // let wrongNum = 0;
+
+        // for (let i = 0; i < array.length; i++) {
+        //     const sampleQuiz = await Quiz.find({num : array[i][0]}); 
+        //     questionArray.push(sampleQuiz[0].question);
+        //     accuracyArray.push(array[i][1]);
+        //     if(array[i][1] == true) correctNum++;
+        //     else wrongNum++;
+        // }
+
 
         let questionArray = []
         let accuracyArray = []
         let correctNum = 0;
         let wrongNum = 0;
 
-        for (let i = 0; i < array.length; i++) {
-            const sampleQuiz = await Quiz.find({num : array[i][0]}); 
+        for (let i = 0; i < newArray.length; i++) {
+            const sampleQuiz = await Quiz.find({num : newArray[i][0]}); 
             questionArray.push(sampleQuiz[0].question);
-            accuracyArray.push(array[i][1]);
-            if(array[i][1] == true) correctNum++;
+            accuracyArray.push(newArray[i][1]);
+            if(newArray[i][1] == true) correctNum++;
             else wrongNum++;
         }
+
+
         console.log(questionArray);
         console.log(accuracyArray);
         console.log(correctNum, wrongNum);
