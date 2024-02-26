e3c7753737a77f7c17ba7c02bd43d176d5eb5b58
chanjin1998 <chanjin@kookmin.ac.kr>
2024-02-26 13:36:34 +0900

    add board

---
+++ financial-cbt-express/models/Board.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const boardSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  content: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  nickname: { type: String, required: true },
+});
+const Board = mongoose.model("Board", boardSchema);
+
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  board: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Board",
+    required: true,
+  },
+  createdAt: { type: Date, default: Date.now },
+  nickname: { type: String, required: true },
+});
+const Comment = mongoose.model("Comment", commentSchema);
+
+module.exports = { Board, Comment };
---
+++ financial-cbt-express/routes/board.js
@@ -0,0 +1,147 @@
+var express = require("express");
+var router = express.Router();
+const { Board, Comment } = require("../models/Board");
+const User = require("../models/User");
+var { verifyToken } = require("../utils/auth");
+
+//전체 게시글 조회
+router.get("/", async (req, res, next) => {
+  const data = await Board.find();
+  res.json(data);
+});
+
+//상세페이지용 특정 게시글 id로 조회
+router.get("/:boardId", async (req, res, next) => {
+  try {
+    const data = await Board.findById(req.params.boardId);
+    console.log(req.params.boardId);
+    if (!data) {
+      return res.status(404).json({ message: "board not found" });
+    }
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//로그인된 사용자만 글을 작성할 수 있게함
+router.post("/", async (req, res, next) => {
+  try {
+    console.log("11", req.body);
+    const board = await Board.create({
+      title: req.body.title,
+      author: req.body.user.user._id,
+      content: req.body.content,
+      nickname: req.body.user.user.nickname,
+    });
+    res.json(board);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//특정 게시글 id로 수정
+router.put("/:boardId", async (req, res, next) => {
+  try {
+    const board = await Board.findByIdAndUpdate(
+      req.params.boardId,
+      {
+        title: req.body.title,
+        content: req.body.content,
+      },
+      { new: true }
+    );
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+    res.json({
+      updatedContent: board,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//특정 게시글 id로 삭제
+router.delete("/:boardId", authenticate, async (req, res, next) => {
+  try {
+    const board = await Board.findByIdAndDelete(req.params.boardId);
+    if (!board) {
+      return res.status(404).json({ message: "Board not found" });
+    }
+    res.json({
+      message: "Board deleted successfully",
+      deletedBoard: board,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//특정 게시글 댓글 조회
+router.get("/:boardId/comment", async (req, res, next) => {
+  try {
+    const data = await Comment.find({ board: req.params.boardId });
+    if (!data) {
+      return res.status(404).json({ message: "comment not found" });
+    }
+    res.json(data);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//특정 게시글 id에 댓글 작성
+router.post("/:boardId/comment", async (req, res, next) => {
+  try {
+    console.log(req.body);
+    const comment = await Comment.create({
+      content: req.body.content,
+      author: req.body.user._id,
+      nickname: req.body.user.nickname,
+      board: req.params.boardId,
+    });
+    console.log("userId", user._id);
+    // console.log("author", author);
+    res.json(comment);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+//특정 댓글 삭제
+router.delete("/:boardId/comment/:commentId", async (req, res, next) => {
+  try {
+    const comment = await Comment.findByIdAndDelete(req.params.commentId);
+    if (!comment) {
+      return res.status(404).json({ message: "Comment not found" });
+    }
+    res.json({
+      message: "Comment deleted successfully",
+      deletedComment: comment,
+    });
+  } catch (err) {
+    console.error(err);
+  }
+});
+// 로그인 체크
+async function authenticate(req, res, next) {
+  let token = req.cookies.authToken;
+  let headerToken = req.headers.authorization;
+  if (!token && headerToken) {
+    token = headerToken.split(" ")[1];
+  }
+
+  const user = verifyToken(token);
+  console.log(user);
+  req.user = user;
+  if (!user) {
+    const error = new Error("Authorization Failed");
+    error.status = 403;
+
+    next(error);
+  }
+  next();
+}
+
+module.exports = router;
